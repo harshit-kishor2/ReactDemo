36d8965378df4d7bf153e5991dad6f5ed021539b
Harshit Kishor <harshitkishore@singsys.com>
2022-11-09 20:04:01 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,7 @@
+import React from 'react'
+import HomeScreen from './Screens/HomeScreen'
+const App = () => {
+  return <HomeScreen />
+}
+
+export default App
---
+++ src/Screens/HomeScreen.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const HomeScreen = () => {
+  return (
+    <div
+      style={{
+        display: 'flex',
+        flexDirection: 'column',
+        backgroundColor: 'whitesmoke',
+        height: '100vh',
+      }}
+    >
+      Hello
+    </div>
+  )
+}
+
+export default HomeScreen
